2af6e2e555aaa4d8dd4e3c75aece73a4777c224b
sibinlal <sbnlalpnr@gmail.com>
2023-10-01 01:26:07 +0530

    updated method to add an array of elements to linkedlist

--- LinkedList/LabLinkedList.java
+++ LinkedList/LabLinkedList.java
@@ -1,167 +1,176 @@
 package com.javalab.ds;
 
 
-
 public class LabLinkedList {
 
-	private Node head;
-	private Node tail;
-	public int size;
+    private Node head;
+    private Node tail;
+    public int size;
 
-	public LabLinkedList() {
-		this.size = 0;
-	}
+    public LabLinkedList() {
+        this.size = 0;
+    }
 
-	public void insertFirst(int value) {
-		Node node = new Node(value);
-		node.next = head;
-		head = node;
+    public void insertAll(int[] values) {
 
-		if (tail == null) {
-			tail = head;
-		}
+        for (int value : values) {
+            insert(value);
+        }
+    }
 
-		size += 1;
-	}
+    public void insertFirst(int value) {
+        Node node = new Node(value);
+        node.next = head;
+        head = node;
 
-	public void insert(int value) {
+        if (tail == null) {
+            tail = head;
+        }
 
-		Node node = new Node(value);
-		if (head == null) { // first element.
-			head = node;
-			tail = head;
-			size += 1;
-			return;
-		}
-		tail.next = node;
-		tail = node;
+        size += 1;
+    }
 
-		size += 1;
-	}
+    public void insert(int value) {
 
-	public void insertToIndex(int value, int index) {
+        Node node = new Node(value);
+        if (head == null) { // first element.
+            head = node;
+            tail = head;
+            size += 1;
+            return;
+        }
+        tail.next = node;
+        tail = node;
 
-		if (index == 0) {
-			insertFirst(value);
-			return;
-		}
+        size += 1;
+    }
 
-		if(index > size){
-			return;
-		}
+    public void insertToIndex(int value, int index) {
 
-		if (index == size) {
-			insert(value);
-			return;
-		}
+        if (index == 0) {
+            insertFirst(value);
+            return;
+        }
 
-		Node temp = head;
-		for (int i = 0; i < index - 1; i++) {
-			temp = temp.next;
-		}
+        if (index > size) {
+            return;
+        }
 
-		Node node = new Node(value, temp.next);
-		temp.next = node;
+        if (index == size) {
+            insert(value);
+            return;
+        }
 
-		size += 1;
-	}
+        Node temp = head;
+        for (int i = 0; i < index - 1; i++) {
+            temp = temp.next;
+        }
 
-	public void deleteFirst() {
-		if (head == null) {
-			return;
-		}
-		if (head.next == null) {
-			head = null;
-			tail = null;
-			size--;
-			return;
-		}
-		head = head.next;
-		size--;
-	}
+        Node node = new Node(value, temp.next);
+        temp.next = node;
 
-	public void deleteLast() {
+        size += 1;
+    }
 
-		if (head == null) {
-			return;
-		}
+    public void deleteFirst() {
+        if (head == null) {
+            return;
+        }
+        if (head.next == null) {
+            head = null;
+            tail = null;
+            size--;
+            return;
+        }
+        head = head.next;
+        size--;
+    }
 
-		if (size == 1) {
-			deleteFirst();
-		}
+    public void deleteLast() {
 
-		Node lastTobeNode = get(size - 2);
-		lastTobeNode.next = null;
-		tail = lastTobeNode;
-		size--;
-	}
+        if (head == null) {
+            return;
+        }
 
-	public int getNodeValue(int index) {
-		return get(index).data;
-	}
+        if (size == 1) {
+            deleteFirst();
+        }
 
-	public Node get(int index) {
+        Node lastTobeNode = get(size - 2);
+        lastTobeNode.next = null;
+        tail = lastTobeNode;
+        size--;
+    }
 
-		if (index == 0) {
-			return head;
-		}
+    public int getNodeValue(int index) {
+        return get(index).data;
+    }
 
-		if (index >= size) {
-			return tail;
-		}
+    public Node get(int index) {
 
-		Node node = head;
-		for (int i = 0; i < index; i++) {
-			node = node.next;
-		}
-		return node;
-	}
+        if (index == 0) {
+            return head;
+        }
 
-	public void display() {
-		Node temp = head;
-		System.out.println("****************");
-		while (temp != null) {
-			System.out.print(" " + temp.data);
-			temp = temp.next;
-		}
-		System.out.println("\n list size is: " + size);
-	}
+        if (index >= size) {
+            return tail;
+        }
 
-	private static class Node {
+        Node node = head;
+        for (int i = 0; i < index; i++) {
+            node = node.next;
+        }
+        return node;
+    }
 
-		private final int data;
-		private Node next;
+    public void display() {
+        Node temp = head;
+        System.out.println("****************");
+        while (temp != null) {
+            System.out.print(" " + temp.data);
+            temp = temp.next;
+        }
+        System.out.println("\n list size is: " + size);
+    }
 
-		public Node(int data) {
-			this.data = data;
-			this.next = null;
-		}
+    private static class Node {
 
-		public Node(int data, Node next) {
-			this.data = data;
-			this.next = next;
-		}
-	}
+        private final int data;
+        private Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+
+        public Node(int data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
 
 }
 
 class Test {
-	public static void main(String[] args){
-		LabLinkedList list = new LabLinkedList();
-		list.insertFirst(5);
-		list.insert(10);
-		list.insert(8);
-		list.display();
+    public static void main(String[] args) {
+        LabLinkedList list = new LabLinkedList();
+        list.insertFirst(5);
+        list.insert(10);
+        list.insert(8);
+        list.display();
 
-		list.deleteFirst();
-		list.display();
+        list.deleteFirst();
+        list.display();
 
-		list.deleteLast();
-		list.display();
+        list.deleteLast();
+        list.display();
 
-		list.insertToIndex(20,1);
+        list.insertToIndex(20, 1);
+        list.display();
+
+		list.insertAll(new int[]{5,6,7});
 		list.display();
-	}
+    }
 }
 
 
